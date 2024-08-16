b127fc167492fb1cffa262dd22ca7d63c79246c1
AppleWind <130741822+KangwonDo1209@users.noreply.github.com>
2024-08-16 18:39:18 +0900

    Indexer
    인덱서

---
+++ Indexer/Indexer/Program.cs
@@ -0,0 +1,33 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+class MyIndexer<Key, Value>
+{
+	private Dictionary<Key, Value> dic;
+
+	public MyIndexer()
+	{
+		dic = new Dictionary<Key, Value>();
+	}
+
+	public Value this[Key key]
+	{
+		get => dic.ContainsKey(key) ? dic[key] : default(Value);
+		set => dic[key] = value;
+	}
+
+}
+
+class Program()
+{
+	static void Main(string[] args)
+	{
+		MyIndexer<string, string> myIndexer = new MyIndexer<string, string>();
+		myIndexer["A"] = "A_Key";
+		myIndexer["B"] = "B_Key";
+
+		Console.WriteLine(myIndexer["A"]);
+		Console.WriteLine(myIndexer["C"]); // 기본값 (공백)
+		Console.WriteLine(myIndexer["B"]);
+
+	}
+}
